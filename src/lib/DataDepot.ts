005f41c3a3f337e44334d1f7b99100f73b7a155f
hj5230 <hf305@foxmail.com>
2024-04-02 19:22:17 +0300

    change module structure

---
+++ src/lib/DataDepot.ts
@@ -0,0 +1,89 @@
+import * as Errors from "./Errors";
+import { Depot } from "./Depot";
+import {
+  ChunkReader,
+  JsonReader,
+  ObjectReader,
+} from "./Reader";
+import { ChunkWriter, JsonWriter } from "./Writer";
+
+export class DataDepot {
+  private constructor() {
+    throw new Errors.InstantiateStaticClassError(
+      "DataDepot"
+    );
+  }
+
+  public static load = <T>(
+    depot: Depot<T>,
+    basePath: string,
+    chunkName: string,
+    key?: string
+  ): void => {
+    depot.load(
+      new ChunkReader<T>(basePath, chunkName).read(key)
+    );
+  };
+
+  public static loadFromJson = <T>(
+    depot: Depot<T>,
+    filePath: string,
+    encoding?: BufferEncoding
+  ): void => {
+    depot.load(
+      new JsonReader<T>(filePath, encoding).read()
+    );
+  };
+
+  public static loadFromObject = <T>(
+    depot: Depot<T>,
+    object: object
+  ) => {
+    depot.load(new ObjectReader<T>(object).read());
+  };
+
+  public static insertAnObject = <T>(
+    depot: Depot<T>,
+    key: string,
+    object: object
+  ) => {
+    depot.setItem(
+      key,
+      new ObjectReader<T>(object).read() as unknown as T
+    );
+  };
+
+  public static write = <T>(
+    depot: Depot<T>,
+    basePath: string,
+    option?: {
+      chunkName?: string;
+      key?: string;
+      maxChunkSize?: number;
+      maxChunkCount?: number;
+    }
+  ): void => {
+    const serialized =
+      option?.key !== undefined
+        ? depot._serialize(option.key)
+        : depot._serialize();
+    if (option) {
+      const { chunkName, maxChunkSize, maxChunkCount } =
+        option;
+      new ChunkWriter(basePath, {
+        chunkName,
+        maxChunkSize,
+        maxChunkCount,
+      }).write(serialized);
+    } else {
+      new ChunkWriter(basePath).write(serialized);
+    }
+  };
+
+  public static writeToJson = <T>(
+    depot: Depot<T>,
+    fileName: string
+  ): void => {
+    new JsonWriter(fileName).write(depot._serialize());
+  };
+}
